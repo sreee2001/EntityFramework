254d8e3dca458f422c1f0d81a67dc69707deefca
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-07-21 13:23:53 -0500

    Example with seeding the DB on creation

---
+++ CodeFirstWithDatabaseSeeding/DBContext/WorldCountries.cs
@@ -0,0 +1,33 @@
+﻿using CodeFirstExample.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExample.DBContext
+{
+    internal class WorldCountries : DbContext
+    {
+        public WorldCountries()
+            //: base("name=dev")
+            //: base("name=qa")
+            //: base("name=prod")            
+        {
+            Database.SetInitializer(new DBInitializer());
+        }
+        public DbSet<Country> Countries { get; set; }
+    }
+
+    internal class DBInitializer : DropCreateDatabaseAlways<WorldCountries>
+    {
+        protected override void Seed(WorldCountries context)
+        {
+            context.Countries.Add(new Country { Name = "United States" });
+            context.Countries.Add(new Country { Name = "Canada" });
+            context.Countries.Add(new Country { Name = "Mexico" });
+            base.Seed(context);
+        }
+    }
+}
---
+++ CodeFirstWithDatabaseSeeding/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new DBContext.WorldCountries())
+            {
+               
+                // Retrieve and display the countries
+                var countries = db.Countries.ToList();
+                foreach (var c in countries)
+                {
+                    Console.WriteLine($"Country: {c.Name}");
+                }
+            }
+        }
+    }
+}
