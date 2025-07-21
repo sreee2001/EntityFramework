b95d5e7c7044bf6f138f9f98bfbfec4841676ac8
Srikanth Tangella <srikanth.tangella@gmail.com>
2025-07-21 07:56:41 -0500

    Initialize the database according to the connectionstring

--- TestingCodeFirst_FirstProject/DBContext/BloggingContext.cs
+++ TestingCodeFirst_FirstProject/DBContext/BloggingContext.cs
@@ -10,6 +10,12 @@ namespace TestingCodeFirst_FirstProject.DBContext
 {
     internal class BloggingContext : DbContext
     {
+        public BloggingContext()
+            : base("name=dev")
+            //: base("name=qa")
+            //: base("name=prod")
+            
+        { }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
     }
